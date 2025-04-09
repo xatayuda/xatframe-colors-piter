592c1c944b9b103fd039056a66370ef42a9907c9
xatayuda <djayudax@gmail.com>
2025-04-09 13:18:23 -0400

    Update script.js
    adding local storage to save color selected

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
         '#448F2E', '#2D9C7A', '#4086B8', '#452EB8', '#A94EB8', '#B84E8C', '#22202B'];
     let indiceColor = 0;
 
+    const colorGuardado = localStorage.getItem('colorFondo');
+    if (colorGuardado && colores.includes(colorGuardado)) {
+      document.body.style.backgroundColor = colorGuardado;
+      indiceColor = colores.indexOf(colorGuardado);
+    }
     document.getElementById('colorButton').addEventListener('click', () => {
-    indiceColor = (indiceColor + 1) % colores.length;
-    document.body.style.backgroundColor = colores[indiceColor];
+      indiceColor = (indiceColor + 1) % colores.length;
+      const nuevoColor = colores[indiceColor];
+      document.body.style.backgroundColor = nuevoColor;
+      localStorage.setItem('colorFondo', nuevoColor);
     });
